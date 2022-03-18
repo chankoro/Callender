afc78959266fbd4fade24530fdf4d9cea9ad8e8f
耀平 山田 <y.yamada@seiken-grp.co.jp>
2022-03-18 18:08:48 +0900

    年計算がおかしい

--- Callender/Program.cs
+++ Callender/Program.cs
@@ -12,8 +12,8 @@ namespace Prob8_14
         { 
             // 年月入力テキストボックス
             //Console.Write("年を入力してください");
-            Console.WriteLine();
-            Console.Write("年:");
+            //Console.WriteLine();
+            //Console.Write("年:");
             // 年を入力
             int year = int.Parse(Console.ReadLine());
             Console.WriteLine("{0}年",year);
@@ -23,34 +23,11 @@ namespace Prob8_14
             //Console.Write("月:");
             //int month = int.Parse(Console.ReadLine());
             //Console.WriteLine("{0}月:",month);
-            // 日を入力
-            //Console.Write("日:");
-            //int day = int.Parse(Console.ReadLine());
-            //Console.WriteLine("{0}日:",day);
-            //Console.WriteLine("{0}年{1}月{2}日",year,month,day);
 
             // 曜日を定義
             int week;
             string[] weekarray = {"日","月","火","水","木","金","土"};
-            // 曜日計算
-            // 1月2月の場合
-            //if(month ==1 || month == 2)
-            //{
-            //    year--;
-            //    month += 12;
-            //    // ツェラーの公式
-            //    week = (year + (year/4) - (year/100) + (year/400) + (13*month+8)/5 + day) % 7;
-            //    Console.WriteLine("week:{0}",week);
-            //    Console.WriteLine("{0}曜日",weekarray[week]);
-            //}
-            //else
-            //{
-            //    // ツェラーの公式
-            //    week = (year + (year/4) - (year/100) + (year/400) + (13*month+8)/5 + day) % 7;
-            //    Console.WriteLine("week:{0}",week);
-            //    Console.WriteLine("{0}曜日",weekarray[week]);
-            //}
-            
+             
             // 閏年かの判定
             if(year % 4 == 0 && year % 100 != 0 )
             {
@@ -72,20 +49,21 @@ namespace Prob8_14
             int []aday = new int[12];
             
             // ループが正常終了するよう、月をmonthとする。
-            int month;
+            //int month;
             for(int i = 1; i <= 12; i++)
             {
+              int year2 = year;
+              int month = 0;
               Console.WriteLine("{0}月",i);
               // 1月の曜日計算が違うので分ける
               if(i == 1)
                 {
                     // ツェラーの公式で1月2月は前年の13月14月で計算
                       year --;
-                    　month = i+12;
+                    　month = 13;
                       for(int j = 1; j < 32; j++)
-                    {
+                      {
                         // ツェラーの公式
-                        
                         week = (year + (year/4) - (year/100) + (year/400) + (13*month+8)/5 + j) % 7;
                         if(j == 1)
                         {
@@ -95,126 +73,179 @@ namespace Prob8_14
                                 Console.Write("  ");
                             }
                         }
-                        if(week == 6)
+                        if(week == 0)
                         {
+                            //日曜日は赤で表示する
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("{0,2:d}",j);
+                        }
+                        else if(week == 6)
+                        {
+                            //土曜日は青で表示する
+                            Console.ForegroundColor = ConsoleColor.Blue;
                             Console.Write("{0,2:d}",j);
                             Console.WriteLine();
                         }
                         else 
                         { 
+                            Console.ForegroundColor = ConsoleColor.Gray;
                             Console.Write("{0,2:d}",j);
                         }
-                    }
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        year2 = year;
+                      }
+                     
                 }
-              // 2月は閏年は29日
+               // 2月は閏年は29日
               else if(i == 2 && urucheck == 1)
                 {
                         year --;
-                        month = i + 12;
-                    for(int j = 1; j <= 29; j++)
-                    {
-                        // ツェラーの公式
-                        week = (year + (year/4) - (year/100) + (year/400) + (13*month+8)/5 + j) % 7;
-                     if(j == 1)
+                        month = 14;
+                        for(int j = 1; j <= 29; j++)
                         {
-                            // 1日の曜日を合わせる
-                            for(int k = 0; k < week; k++)
-                            {
-                                Console.Write("  ");
-                            }
+                             // ツェラーの公式
+                             week = (year + (year/4) - (year/100) + (year/400) + (13*month+8)/5 + j) % 7;
+                          if(j == 1)
+                          {
+                              // 1日の曜日を合わせる
+                              for(int k = 0; k < week; k++)
+                              {
+                                  Console.Write("  ");
+                              }
+                          }
+                          if(week == 0)
+                          {
+                              //日曜日は赤で表示する
+                              Console.ForegroundColor = ConsoleColor.Red;
+                              Console.Write("{0,2:d}",j);
+                          }
+                          else if(week == 6)
+                          {
+                              //土曜日は青で表示する
+                              Console.ForegroundColor = ConsoleColor.Blue;
+                              Console.Write("{0,2:d}",j);
+                              Console.WriteLine();
+                          }
+                          else 
+                          { 
+                              Console.ForegroundColor = ConsoleColor.Gray;
+                              Console.Write("{0,2:d}",j);
+                          }
+                          Console.ForegroundColor = ConsoleColor.Gray;
                         }
-                        if(week == 6)
-                        {
-                            Console.Write("{0,2:d}",j);
-                            Console.WriteLine();
-                        }
-                        else 
-                        { 
-                            Console.Write("{0,2:d}",j);
-                        }
-                    }
                 }
               // 2月は閏年でなければ28日
               else if(i== 2 && urucheck == 0)
                 {
                         year --;
-                        month = i + 12;
-                      for(int j = 1; j <= 28; j++)
-                    {
-                        // ツェラーの公式
-                        week = (year + (year/4) - (year/100) + (year/400) + (((13*month)+8)/5) + j) % 7;
-                       if(j == 1)
+                        month = 14;
+                        for(int j = 1; j <= 28; j++)
                         {
-                            // 1日の曜日を合わせる
-                            for(int k = 0; k < week; k++)
-                            {
-                                Console.Write("  ");
+                            // ツェラーの公式
+                            week = (year + (year/4) - (year/100) + (year/400) + (((13*month)+8)/5) + j) % 7;
+                            // 1日の曜日を合わせる    
+                        if(j == 1) 
+                            { 
+                               for(int k = 0; k < week; k++)
+                               {
+                                   Console.Write("  ");
+                               }
                             }
+                            if(week == 0)
+                               {
+                                  //日曜日は赤で表示する
+                                  Console.ForegroundColor = ConsoleColor.Red;
+                                  Console.Write("{0,2:d}",j);
+                                }
+                            else if(week == 6)
+                                {
+                                   //土曜日は青で表示する
+                                   Console.ForegroundColor = ConsoleColor.Blue;
+                                   Console.Write("{0,2:d}",j);
+                                   Console.WriteLine();
+                                 }
+                            else
+                                 {  
+                                   Console.ForegroundColor = ConsoleColor.Gray;
+                                   Console.Write("{0,2:d}",j);
+                                  }
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            year2 = year;
                         }
-                        if(week == 6)
-                        {
-                            Console.Write("{0,2:d}",j);
-                            Console.WriteLine();
-                        }
-                        else 
-                        { 
-                            Console.Write("{0,2:d}",j);
-                        }
-                    }
                 }
-               // 4月,6月,9月,11月は30日
+              // 4月,6月,9月,11月は30日
               else if(i == 4 || i == 6 || i == 9 || i == 11)
                 {
-                    for (int j = 1; j < 31; j++)
-                    {
-                         month = i;
-                         // ツェラーの公式
-                         week = (year + (year/4) - (year/100) + (year/400) + (13*month+8)/5 + j) % 7;
-                       if(j == 1)
+                        for (int j = 1; j < 31; j++)
                         {
+                            month = i;
+                            // ツェラーの公式
+                            week = (year + (year/4) - (year/100) + (year/400) + (13*month+8)/5 + j) % 7;
                             // 1日の曜日を合わせる
-                            for(int k = 0; k < week; k++)
+                             if(j == 1) 
+                             { 
+                               for(int k = 0; k < week; k++)
+                               {
+                                    Console.Write("  ");
+                               }
+                             }
+                        if(week == 0)
                             {
-                                Console.Write("  ");
+                               //日曜日は赤で表示する
+                               Console.ForegroundColor = ConsoleColor.Red;
+                               Console.Write("{0,2:d}",j);
                             }
+                        else if(week == 6)
+                            {
+                               //土曜日は青で表示する
+                               Console.ForegroundColor = ConsoleColor.Blue;
+                               Console.Write("{0,2:d}",j);
+                               Console.WriteLine();
+                             }
+                        else
+                             {  
+                               Console.ForegroundColor = ConsoleColor.Gray;
+                               Console.Write("{0,2:d}",j);
+                             }
+                        Console.ForegroundColor = ConsoleColor.Gray;
                         }
-                        if(week == 6)
-                        {
-                            Console.Write("{0,2:d}",j);
-                            Console.WriteLine();
-                        }
-                        else 
-                        { 
-                            Console.Write("{0,2:d}",j);
-                        }
-                   }
                 }
               // それ以外は31日
               else 
                 { 
-                    for(int j = 1; j < 32; j++)
-                    {
-                        month = i;
-                        // ツェラーの公式
-                        week = (year + (year/4) - (year/100) + (year/400) + (13*month+8)/5 + j) % 7;
-                       if(j == 1)
+                        for(int j = 1; j < 32; j++)
                         {
-                            // 1日の曜日を合わせる
-                            for(int k = 0; k < week; k++)
+                            month = i;
+                            // ツェラーの公式
+                            week = (year + (year/4) - (year/100) + (year/400) + (13*month+8)/5 + j) % 7;
+                              // 1日の曜日を合わせる
+                             if(j == 1) 
+                             { 
+                               for(int k = 0; k < week; k++)
+                               {
+                                    Console.Write("  ");
+                               }
+                             }
+                        if(week == 0)
                             {
-                                Console.Write("  ");
+                               //日曜日は赤で表示する
+                               Console.ForegroundColor = ConsoleColor.Red;
+                               Console.Write("{0,2:d}",j);
                             }
+                        else if(week == 6)
+                            {
+                               //土曜日は青で表示する
+                               Console.ForegroundColor = ConsoleColor.Blue;
+                               Console.Write("{0,2:d}",j);
+                               Console.WriteLine();
+                             }
+                        else
+                             {  
+                               Console.ForegroundColor = ConsoleColor.Gray;
+                               Console.Write("{0,2:d}",j);
+                             }
+                        Console.ForegroundColor = ConsoleColor.Gray;
                         }
-                        if(week == 6)
-                        {
-                            Console.Write("{0,2:d}",j);
-                            Console.WriteLine();
-                        }
-                        else 
-                        { 
-                            Console.Write("{0,2:d}",j);
-                        }
-                    }
                 }
                 Console.WriteLine();
                 Console.WriteLine();
